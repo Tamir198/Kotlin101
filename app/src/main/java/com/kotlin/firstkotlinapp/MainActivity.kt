483105c1a434477b99bd0fd7256c71ccee30d87f
Tamir <1tamir198@gmail.com>
2019-08-27 20:09:37 +0300

    Type checking

--- app/src/main/java/com/kotlin/firstkotlinapp/MainActivity.kt
+++ app/src/main/java/com/kotlin/firstkotlinapp/MainActivity.kt
@@ -2,7 +2,6 @@ package com.kotlin.firstkotlinapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,10 +9,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        printsAndFun()
 
 
-        /////
+        typeChecking()
+
 
 
     }
@@ -33,9 +32,29 @@ class MainActivity : AppCompatActivity() {
 
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-        
+
         val num1:Double = 1.111111
         val num2:Double = 2.222222
         println("num1 and num2 sum is    + ${num1 + num2}") //${} is for arithmetic calculations inside the print
     }
+
+
+    fun typeChecking(){
+        if(true is Boolean){
+            println("You can check is a is double or any other  type using is inside if")
+        }
+
+        val doubleValueCheck = 4.0
+        if(doubleValueCheck is Double){
+            println("doubleValueCheck is sure a double")
+        }
+
+        val stringForCheck = "Hummus"
+        if(stringForCheck is String){
+            println("stringForCheck is made of Hummus? wait... its a string not a hummus")
+        }
+    }
+
+
+
 }
