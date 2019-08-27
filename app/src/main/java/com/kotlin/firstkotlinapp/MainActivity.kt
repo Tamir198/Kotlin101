b4a0f9861cce53fdc55921e0557296977486e9e4
Tamir <1tamir198@gmail.com>
2019-08-27 22:26:30 +0300

    Casting and multiLine string

--- app/src/main/java/com/kotlin/firstkotlinapp/MainActivity.kt
+++ app/src/main/java/com/kotlin/firstkotlinapp/MainActivity.kt
@@ -2,6 +2,16 @@ package com.kotlin.firstkotlinapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import java.util.logging.Logger
+import android.R
+import androidx.core.app.ComponentActivity
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,7 +21,6 @@ class MainActivity : AppCompatActivity() {
 
 
 
-        typeChecking()
 
 
 
@@ -33,28 +42,54 @@ class MainActivity : AppCompatActivity() {
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-        val num1:Double = 1.111111
-        val num2:Double = 2.222222
+        val num1: Double = 1.111111
+        val num2: Double = 2.222222
         println("num1 and num2 sum is    + ${num1 + num2}") //${} is for arithmetic calculations inside the print
+
+
+        val multiLineString = """
+           |First line
+           |Another line
+           |one more
+           |and stop
+           """.trimMargin()
+
+
+        println("In kotlin you can make multyLine string but you will need ot log it (Log.v)   $multiLineString")
+        Log.v(" text wont with print", "" + multiLineString)
+        Log.wtf(" text wont with print", "" + multiLineString)
+
+
     }
 
 
-    fun typeChecking(){
-        if(true is Boolean){
+    fun typeChecking() {
+        if (true is Boolean) {
             println("You can check is a is double or any other  type using is inside if")
         }
 
         val doubleValueCheck = 4.0
-        if(doubleValueCheck is Double){
+        if (doubleValueCheck is Double) {
             println("doubleValueCheck is sure a double")
         }
 
         val stringForCheck = "Hummus"
-        if(stringForCheck is String){
+        if (stringForCheck is String) {
             println("stringForCheck is made of Hummus? wait... its a string not a hummus")
         }
     }
 
+    fun custing() {
+        var someChar: Char = 'a' //"a" will not compile because if you want a single char you need to wrap it with a single '
+        println("Is a a char tho?  ${someChar is Char}")//How cool is that, this "${someChar is Char}" can go inside a print
+
+        println("Lets make 3.14  Int ${3.14.toInt()}")//It will get rounded
+        println("Lets make A to Int ${'A'.toInt()}") //Value it taken from ascii table
+        println("Lets make 65 to Char ${65.toChar()}") //Value is taken from ascii table
+
+
+
+    }
 
 
 }
